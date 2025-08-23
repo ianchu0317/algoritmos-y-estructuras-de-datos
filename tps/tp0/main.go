0a236bb05e24e76049fad46901573a4898cd696b
Ian Chen <chenian317@gmail.com>
2025-08-23 18:30:33 -0300

    chequear ejercicio 1

--- tps/tp0/main.go
+++ tps/tp0/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	"./ejercicios"
+	"tp0/ejercicios"
 )
 
 func main() {
+	// Verificación de Swap
 	a, b := 5, 6
 	fmt.Println("Las variables son a, b=", a, b)
 	ejercicios.Swap(&a, &b)
 	fmt.Println("Las variables son a, b=", a, b)
+
 }
