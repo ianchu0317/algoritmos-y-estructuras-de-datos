f162b022b38e28a87bfdbebdcb1d6bb3b337ae34
Ian Chen <chenian317@gmail.com>
2025-09-04 17:02:34 -0300

    tests para desapilar pilas

--- tdas/pila/pila_test.go
+++ tdas/pila/pila_test.go
@@ -36,6 +36,23 @@ func TestApilar(t *testing.T) {
 	require.False(t, pilaFlotantes.EstaVacia(), "Pila de flotantes apilados no puede estar vacia")
 	require.False(t, pilaCadenas.EstaVacia(), "Pila de cadenas apilaods no puede que estar vacia")
 	require.Equal(t, 1, pilaEnteros.VerTope(), "Tope de pila de enteros tiene que coincidir con ultimo ingresado")
-	require.Equal(t, 1.1, pilaEnteros.VerTope(), "Tope de pila de flotantes tiene que coincidir con ultimo ingresado")
+	require.Equal(t, 1.1, pilaFlotantes.VerTope(), "Tope de pila de flotantes tiene que coincidir con ultimo ingresado")
 	require.Equal(t, "palabra", pilaCadenas.VerTope(), "Tope de pila de cadenas tiene que coincidir con ultimo ingresado")
+}
+
+func TestDesapilar(t *testing.T) {
+	pilaEnteros := TDAPila.CrearPilaDinamica[int]()
+	pilaFlotantes := TDAPila.CrearPilaDinamica[float32]()
+	pilaCadenas := TDAPila.CrearPilaDinamica[string]()
+
+	pilaEnteros.Apilar(1)
+	pilaFlotantes.Apilar(1.1)
+	pilaCadenas.Apilar("palabra")
+
+	require.Equal(t, 1, pilaEnteros.Desapilar(), "Desapilar pila de enteros tiene que devolver elemento desapilado")
+	require.Equal(t, 1.1, pilaFlotantes.Desapilar(), "Desapilar pila de flotantes tiene que devolver elemento desapilado")
+	require.Equal(t, "palabra", pilaCadenas.Desapilar(), "Desapilar pila de cadenas tiene que devolver elemento desapilado")
+	require.True(t, pilaEnteros.EstaVacia(), "Pila de numeros enteros apilados y desapilados tiene que estar vacia")
+	require.True(t, pilaFlotantes.EstaVacia(), "Pila de numeros flotantes apilados y desapilados tiene que estar vacia")
+	require.True(t, pilaCadenas.EstaVacia(), "Pila de cadenas apilados y desapilados tiene que estar vacia")
 }
