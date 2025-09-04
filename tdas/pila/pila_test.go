0bb9ba520f9080e683b1dac185a4de5831d5327a
Ian Chen <chenian317@gmail.com>
2025-09-04 16:27:28 -0300

    tests de pila vacia con int string float32

--- tdas/pila/pila_test.go
+++ tdas/pila/pila_test.go
@@ -8,7 +8,17 @@ import (
 )
 
 func TestPilaVacia(t *testing.T) {
-	pila := TDAPila.CrearPilaDinamica[int]()
-	require.True(t, pila.EstaVacia())
-	// mas pruebas para este caso...
+	pilaEnteros := TDAPila.CrearPilaDinamica[int]()
+	pilaFlotantes := TDAPila.CrearPilaDinamica[float32]()
+	pilaCadenas := TDAPila.CrearPilaDinamica[string]()
+
+	require.True(t, pilaEnteros.EstaVacia(), "Pila de numeros enteros recien creada tiene que estar vacia")
+	require.True(t, pilaFlotantes.EstaVacia(), "Pila de numeros flotantes recien creada tiene que estar vacia")
+	require.True(t, pilaCadenas.EstaVacia(), "Pila de cadenas recien creada tiene que estar vacia")
+	require.Panics(t, func() { pilaEnteros.VerTope() }, "Pila de numeros enteros recien creada no puede ver tope")
+	require.Panics(t, func() { pilaFlotantes.VerTope() }, "Pila de numeros flotantes recien creada no puede ver tope")
+	require.Panics(t, func() { pilaCadenas.VerTope() }, "Pila de cadenas recien creada no puede ver tope")
+	require.Panics(t, func() { pilaEnteros.Desapilar() }, "Pila de numeros enteros recien creada no puede desapilar")
+	require.Panics(t, func() { pilaFlotantes.Desapilar() }, "Pila de numeros flotantes recien creada no puede desapilar")
+	require.Panics(t, func() { pilaCadenas.Desapilar() }, "Pila de cadenas recien creada no puede desapilar")
 }
