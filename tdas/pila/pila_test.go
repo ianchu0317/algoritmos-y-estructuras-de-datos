55c0af63a12e6b4002f7fcd0954c0ee1d615e906
Ian Chen <chenian317@gmail.com>
2025-09-04 16:51:59 -0300

    tests para apilar pilas

--- tdas/pila/pila_test.go
+++ tdas/pila/pila_test.go
@@ -22,3 +22,20 @@ func TestPilaVacia(t *testing.T) {
 	require.Panics(t, func() { pilaFlotantes.Desapilar() }, "Pila de numeros flotantes recien creada no puede desapilar")
 	require.Panics(t, func() { pilaCadenas.Desapilar() }, "Pila de cadenas recien creada no puede desapilar")
 }
+
+func TestApilar(t *testing.T) {
+	pilaEnteros := TDAPila.CrearPilaDinamica[int]()
+	pilaFlotantes := TDAPila.CrearPilaDinamica[float32]()
+	pilaCadenas := TDAPila.CrearPilaDinamica[string]()
+
+	pilaEnteros.Apilar(1)
+	pilaFlotantes.Apilar(1.1)
+	pilaCadenas.Apilar("palabra")
+
+	require.False(t, pilaEnteros.EstaVacia(), "Pila de enteros apilados no puede estar vacia")
+	require.False(t, pilaFlotantes.EstaVacia(), "Pila de flotantes apilados no puede estar vacia")
+	require.False(t, pilaCadenas.EstaVacia(), "Pila de cadenas apilaods no puede que estar vacia")
+	require.Equal(t, 1, pilaEnteros.VerTope(), "Tope de pila de enteros tiene que coincidir con ultimo ingresado")
+	require.Equal(t, 1.1, pilaEnteros.VerTope(), "Tope de pila de flotantes tiene que coincidir con ultimo ingresado")
+	require.Equal(t, "palabra", pilaCadenas.VerTope(), "Tope de pila de cadenas tiene que coincidir con ultimo ingresado")
+}
