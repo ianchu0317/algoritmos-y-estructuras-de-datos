7342ce4ff353dabb7188d330c6587ec7757e3528
Ian Chen <chenian317@gmail.com>
2025-09-09 11:02:44 -0300

    crear pruebas simples para crear cola enlazada

--- tdas/cola/cola_enlazada.go
+++ tdas/cola/cola_enlazada.go
@@ -10,16 +10,21 @@ type ColaEnlazada[T any] struct {
 	ultimo  *Nodo[T]
 }
 
-// EstaVacia devuelve verdadero si la cola no tiene elementos encolados, false en caso contrario.
 func (c ColaEnlazada[T]) EstaVacia() bool {
 	return c.primero == nil && c.ultimo == nil
 }
 
-func (c ColaEnlazada[T]) VerPrimero() T
+func (c ColaEnlazada[T]) VerPrimero() T {
+	return c.primero.dato
+}
 
-func (c *ColaEnlazada[T]) Encolar(T)
+func (c *ColaEnlazada[T]) Encolar(T) {
 
-func (c *ColaEnlazada[T]) Desencolar() T
+}
+
+func (c *ColaEnlazada[T]) Desencolar() T {
+	return c.primero.dato
+}
 
 func CrearColaEnlazada[T any]() Cola[T] {
 	nuevaCola := ColaEnlazada[T]{primero: nil, ultimo: nil}
--- tdas/cola/cola_test.go
+++ tdas/cola/cola_test.go
@@ -1,1 +1,19 @@
 package cola_test
+
+import (
+	TDACola "tdas/cola"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestColaVacia(t *testing.T) {
+	colaEnteros := TDACola.CrearColaEnlazada[int]()
+	colaFlotantes := TDACola.CrearColaEnlazada[float64]()
+	colaCadenas := TDACola.CrearColaEnlazada[string]()
+
+	require.True(t, colaEnteros.EstaVacia(), "Cola de enteros recien creada deberia estar vacia")
+	require.True(t, colaFlotantes.EstaVacia(), "Cola de floantes recien creada deberia estar vacia")
+	require.True(t, colaCadenas.EstaVacia(), "Cola de cadenas recien creada deberia estar vacia")
+
+}
