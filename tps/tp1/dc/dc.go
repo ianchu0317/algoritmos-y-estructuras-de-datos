5605ae5e820fd51508ccf3a5ba942975d0934114
Ian Chen <chenian317@gmail.com>
2025-09-16 11:16:47 -0300

    base de scanner en cada linea

--- tps/tp1/dc/dc.go
+++ tps/tp1/dc/dc.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
+	// leer args
 	args := os.Args
 	fmt.Println(args)
+
+	// crear un scanner y leer stdin
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println("Linea es:", scanner.Text())
+	}
 }
