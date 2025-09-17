087aa117cdfad8221e0053d1ebdf50688eff1f23
Ian Chen <chenian317@gmail.com>
2025-09-17 17:11:00 -0300

    ejercicio elemento pico

--- guias/division_y_conquista/tests.py
+++ guias/division_y_conquista/tests.py
@@ -29,7 +29,13 @@ def elementoDesordenado(arr):
         return elementoDesordenado(arr[:mitad])
     else if not der_ordenado:
         return"""
+print(lista)
+print("Esta ordenado: ", estaOrdenado(lista, len(lista)))
+print(lista2)
+print("Esta ordenado: ", estaOrdenado(lista2, len(lista2)))
 
+
+# Ejercicio 12 hallar raiz cuadrada (parte entera)
 def raizCuadrada(n, inicio, final):
     mitad = (inicio + final) // 2
     if mitad**2 <= n and (mitad+1)**2 > n:
@@ -39,9 +45,24 @@ def raizCuadrada(n, inicio, final):
             return raizCuadrada(n, mitad+1, final)
         else:
             return raizCuadrada(n, inicio, mitad-1)
-    
-print(lista)
-print("Esta ordenado: ", estaOrdenado(lista, len(lista)))
-print(lista2)
-print("Esta ordenado: ", estaOrdenado(lista2, len(lista2)))
+
 print("Raiz cuadrada de ", raizCuadrada(25, 1, 25))
+
+
+# Ejercicio 13 hallar posicion de elemento pico
+def posicionPico(arr, inicio, final):
+    mitad = (inicio + final) // 2
+    print(arr[mitad])
+    # Caso base
+    if arr[mitad-1] < arr[mitad] > arr[mitad+1]:
+        return mitad
+    else:
+        if arr[mitad-1] > arr[mitad]:
+            return posicionPico(arr, inicio, mitad-1)
+        elif arr[mitad+1] > arr[mitad]:
+            return posicionPico(arr, mitad+1, inicio)
+
+lista_pico = [1, 2, 3, 1, 0, -2, -1, -50]
+print("Posicion pico es: ", posicionPico(lista_pico, 0, len(lista_pico)-1))
+
+
