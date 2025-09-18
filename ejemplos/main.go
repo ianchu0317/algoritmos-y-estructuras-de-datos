8d74e590f718c2b4c6b244ce79aecb016f142929
Ian Chen <chenian317@gmail.com>
2025-09-18 18:16:09 -0300

    terminar counting sort

--- ejemplos/main.go
+++ ejemplos/main.go
@@ -10,19 +10,23 @@ func main() {
 	// Merge sort
 	// O(n log(n))
 	arr1 := aux.CrearArrayRandom(10)
+	fmt.Println("\n***Merge Sort****")
 	fmt.Println("Nuevo array es:", arr1)
 	fmt.Println("Array ordenado con Merge Sort es:", sort.MergeSort(arr1))
 
 	// Quick sort
 	// O(n log(n))
-	arr2 := make([]int, len(arr1))
-	copy(arr2, arr1)
-	fmt.Println(arr2)
+	arr2 := aux.CrearArrayRandom(10)
+	fmt.Println("\n***Quick Sort****")
+	fmt.Println("\nArray es:", arr2)
 	sort.QuickSort(arr2)
 	fmt.Println("Array ordenado con Quick Sort es:", arr2)
 
 	// Counting Sort
 	// Como sabemos que arreglo va de 0 a 100
 	// Podemos ordenar con counting sort ya que es rango acotado
-
+	arr3 := aux.CrearArrayRandom(10)
+	fmt.Println("\n***Counting Sort****")
+	fmt.Println("Nuevo array es:", arr3)
+	fmt.Println("Array ordenado con Counting Sort es:", sort.CountingSort(arr3))
 }
--- ejemplos/ordenamientos/counting_sort.go
+++ ejemplos/ordenamientos/counting_sort.go
@@ -4,15 +4,28 @@ package ordenamientos
 // NO In-place
 // Estable
 
+const RANGO = 101
+
 func CountingSort(arr []int) []int {
-	// Crear array de frecuencias
-	contador := make([]int, 101)
+	// 1) Crear array de frecuencias
+	contador := make([]int, RANGO)
 	for _, num := range arr {
 		contador[num] += 1
 	}
-	// Crear array de inicios
-	inicios := make([]int, 101)
-	for i := range inicios {
-		inicios[i]
+	//fmt.Println("Contador:", contador)
+	// 2) Crear array de inicios
+	inicios := make([]int, RANGO)
+	for i := 1; i < RANGO; i++ {
+		inicios[i] = inicios[i-1] + contador[i-1]
 	}
+	//fmt.Println("Inicios:", inicios)
+	// 3) Devolver en nuevo array
+	nuevoArr := make([]int, len(arr))
+	for i := range arr {
+		num := arr[i]
+		posicion := inicios[num] // Obtener la posicion de inicio
+		nuevoArr[posicion] = num // Colocar en su posicion de inicio
+		inicios[num]++           // Cambiar su posicion de inicio una vez colocado
+	}
+	return nuevoArr
 }
