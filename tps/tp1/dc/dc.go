06546c1f51275d77e0f0c21ec9e550821a19fb41
Ian Chen <chenian317@gmail.com>
2025-09-19 10:37:00 -0300

    split la cadena en distintos char

--- tps/tp1/dc/dc.go
+++ tps/tp1/dc/dc.go
@@ -6,13 +6,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+// extraerSiguienteDigito
+// func extraerSiguienteDigito
+
+// calcularOperacion calcula la operación en formato polaco inverso
+func calcularOperacion(linea string) {
+	miArrayDeLetras := strings.Fields(linea)
+	fmt.Println(miArrayDeLetras)
+}
 
 func main() {
 	// crear un scanner y leer stdin
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		lineaActual := s.Text()
-		fmt.Println("Linea es:", lineaActual)
+		fmt.Println("\nLinea es:", lineaActual)
+		calcularOperacion(lineaActual)
 	}
 }
