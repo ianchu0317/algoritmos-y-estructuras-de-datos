2682844853c0f888ab022c78eb81d188a1d9776f
Ian Chen <chenian317@gmail.com>
2025-09-27 16:05:02 -0300

    ideas para tests de lista

--- tdas/lista/lista_test.go
+++ tdas/lista/lista_test.go
@@ -1,1 +1,18 @@
 package lista_test
+
+// Test de TDA Lista sin iterador
+// - Caso lista vacia
+// - Caso lista con un solo elemento
+// - Casos especificos, entrada y salida
+
+// Test iterador interno
+// - Test por false (idea numeros pares)
+// - Test por true
+
+// Test iterador externo
+// - Insertar primer elemento de iterador
+// - Insertar elemento e interador al final
+// - Insertar elemento en medio de iterador, en su posicion correcta
+// - Remover primer elemento, cambia lista original
+// - Verificar que remover elemento del medio no esta
+// - Casos borde espeficiso
