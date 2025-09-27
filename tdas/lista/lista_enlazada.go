e95d699244e5bac70e07e0d19adf9848086dc61a
Ian Chen <chenian317@gmail.com>
2025-09-27 15:58:52 -0300

    agregar estructura general de lista enlazada e iterador externo

--- tdas/lista/lista_enlazada.go
+++ tdas/lista/lista_enlazada.go
@@ -1,1 +1,79 @@
 package lista
+
+// *** Declarar estructuras para Lista Enlazada ***
+
+type nodo[T any] struct {
+	dato      T
+	siguiente *nodo[T]
+}
+
+type listaEnlazada[T any] struct {
+	primero *nodo[T]
+	ultimo  *nodo[T]
+	largo   int
+}
+
+func CrearListaEnlazada[T any]() Lista[T] {
+	nuevaLista := listaEnlazada[T]{nil, nil, 0}
+	return &nuevaLista
+}
+
+// Primitivas Lista Enlazada
+
+func (lista listaEnlazada[T]) EstaVacia() bool {
+	return lista.largo == 0
+}
+
+func (lista listaEnlazada[T]) InsertarPrimero(elemento T) {
+
+}
+
+func (lista listaEnlazada[T]) InsertarUltimo(elemento T) {
+
+}
+
+func (lista listaEnlazada[T]) BorrarPrimero() T {
+
+}
+
+func (lista listaEnlazada[T]) VerPrimero() T {
+
+}
+
+func (lista listaEnlazada[T]) VerUltimo() T {
+
+}
+
+func (lista listaEnlazada[T]) Largo() int {
+	return lista.largo
+}
+
+func (lista listaEnlazada[T]) Iterar(visitar func(T) bool) {
+
+}
+
+func (lista listaEnlazada[T]) Iterador() IteradorLista[T] {
+
+}
+
+// *** Declarar primitivas y estructuras de iterador externo ***
+type iteradorLista[T any] struct {
+	actual   *nodo[T]
+	anterior *nodo[T]
+}
+
+func (iter iteradorLista[T]) HaySiguiente() bool {
+
+}
+
+func (iter iteradorLista[T]) Siguiente() {
+
+}
+
+func (iter iteradorLista[T]) Insertar(elemento T) {
+
+}
+
+func (iter iteradorLista[T]) Borrar() T {
+
+}
