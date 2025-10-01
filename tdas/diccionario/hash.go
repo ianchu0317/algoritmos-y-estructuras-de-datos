1f3534d145eb0297110417d2e50c19a69a4a2eab
Ian Chen <chenian317@gmail.com>
2025-10-01 19:04:26 -0300

    añadir implementacion para hashear claves

--- tdas/diccionario/hash.go
+++ tdas/diccionario/hash.go
@@ -1,6 +1,9 @@
 package diccionario
 
-import TDALista "tdas/lista"
+import (
+	"fmt"
+	TDALista "tdas/lista"
+)
 
 // *** Estructura de hash abierto ***
 
@@ -13,3 +16,24 @@ type hashAbierto[K any, V any] struct {
 	arreglo  []TDALista.Lista[celda[K, V]]
 	cantidad int
 }
+
+// Funciones auxiliares
+
+// convertirABytes toma la clave y la convierte a bytes
+func convertirABytes[K any](clave K) []byte {
+	return []byte(fmt.Sprintf("%v", clave))
+}
+
+// djb2Hash toma una clave y el numero maximo que puede valer,
+// y devuelve la clave en hash dentro del rango
+// documentacion http://www.cse.yorku.ca/~oz/hash.html
+func Djb2Hash[K any](clave K, largo int) int {
+	bytes := convertirABytes(clave)
+	hash := 5381
+	for _, b := range bytes {
+		hash = ((hash << 5) + hash) + int(b) // hash * 33 + b
+	}
+	return hash % largo
+}
+
+// Primitivas de hash abierto
