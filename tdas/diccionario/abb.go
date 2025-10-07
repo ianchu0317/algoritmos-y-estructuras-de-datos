3153f4d520215af81983ab4a0688b9e30f8b71e8
Ian Chen <chenian317@gmail.com>
2025-10-07 15:33:36 -0300

    funciones auxiliares para abb

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -1,5 +1,7 @@
 package diccionario
 
+import "fmt"
+
 // *** Estructura de ABB ***
 
 type nodo[K any, V any] struct {
@@ -13,3 +15,28 @@ type arbolBinario[K any, V any] struct {
 	raiz     *nodo[K, V]
 	comparar func(K, K) int
 }
+
+// Funciones auxiliares
+
+// crearNodo crea un nodo para el abb
+func crearNodo[K any, V any](clave K, dato V) *nodo[K, V] {
+	return &nodo[K, V]{clave: clave, dato: dato, izq: nil, der: nil}
+}
+
+// convertirBytes toma la clave y la convierte a bytes
+func convertirBytes[K any](clave K) []byte {
+	return []byte(fmt.Sprintf("%v", clave))
+}
+
+// djb2HashInt toma una clave y devuelve un entero unico con respecto a la clave.
+// documentacion http://www.cse.yorku.ca/~oz/hash.html
+func djb2HashInt[K any](clave K) int {
+	bytes := convertirBytes(clave)
+	hash := uint32(5381)
+	for _, b := range bytes {
+		hash = ((hash << 5) + hash) + uint32(b) // hash*33 + b
+	}
+	return int(hash)
+}
+
+// Primitivas de ABB
