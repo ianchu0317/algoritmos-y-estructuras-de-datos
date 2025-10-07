74ca1a0286cd37bafb4f104687d572f1e90db571
Ian Chen <chenian317@gmail.com>
2025-10-07 15:24:08 -0300

    preparar estructura abb a utilizar

--- tdas/diccionario/abb.go
+++ tdas/diccionario/abb.go
@@ -1,1 +1,15 @@
 package diccionario
+
+// *** Estructura de ABB ***
+
+type nodo[K any, V any] struct {
+	clave K
+	dato  V
+	izq   *nodo[K, V]
+	der   *nodo[K, V]
+}
+
+type arbolBinario[K any, V any] struct {
+	raiz     *nodo[K, V]
+	comparar func(K, K) int
+}
