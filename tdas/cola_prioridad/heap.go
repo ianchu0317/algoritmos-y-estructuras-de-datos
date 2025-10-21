6f46c5dd9b128dda3e070e11e0ef9c28b12927ca
Ian Chen <chenian317@gmail.com>
2025-10-21 15:56:58 -0300

    organizar primitivas de heap a implementar·

--- tdas/cola_prioridad/heap.go
+++ tdas/cola_prioridad/heap.go
@@ -11,6 +11,39 @@ type heapArr[T any] struct {
 
 // *** Primitivas Heap ***
 
+func (heap heapArr[T]) EstaVacia() bool {
+	return heap.cantidad == 0
+}
+
+func (heap *heapArr[T]) Encolar(T) {
+
+}
+
+func (heap heapArr[T]) VerMax() T {
+	return heap.arr[0]
+}
+
+func (heap heapArr[T]) Desencolar() T {
+	return heap.arr[0]
+}
+
+func (heap heapArr[T]) Cantidad() int {
+	return heap.cantidad
+}
+
+// *** Funciones de creacion de Heap (Constructores)***
+func CrearHeap[T any](funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	heap := heapArr[T]{cantidad: 0, cmp: funcion_cmp}
+	heap.redimensionar(0)
+	return &heap
+}
+
+func CrearHeapArr[T any](arreglo []T, funcion_cmp func(T, T) int) ColaPrioridad[T] {
+	heapify(arreglo, funcion_cmp)
+	heap := heapArr[T]{cantidad: 0, cmp: funcion_cmp}
+	return &heap
+}
+
 // *** HeapSort ***
 
 // *** Funciones auxiliares ***
@@ -35,6 +68,6 @@ func upHeap[T any](pos int, arreglo []T) {
 
 // heapify(arreglo, funcion) toma un arreglo y una funcion de comparacion
 // y modifica el arreglo en forma de heap segun la funcion pasada
-func heapify[T any](arreglo []T, cmp func()) {
+func heapify[T any](arreglo []T, cmp func(T, T) int) {
 
 }
