cd9f26f3ebd6a1dee75c20e277407424ac3b904c
Ian Chen <chenian317@gmail.com>
2025-10-22 16:16:58 -0300

    modularizar tests de heap vacio

--- tdas/cola_prioridad/cola_prioridad_test.go
+++ tdas/cola_prioridad/cola_prioridad_test.go
@@ -28,12 +28,17 @@ func cmpIntMin(a, b int) int {
 }
 
 // Test heap vacio
+// testsHeapVacio corre los tests de heap vacio
+func testsHeapVacio[T any](t *testing.T, heap TDAColaPrioridad.ColaPrioridad[T]) {
+	require.Equal(t, 0, heap.Cantidad(), "Heap vacio debe tener cantidad 0")
+	require.True(t, heap.EstaVacia(), "Heap vacio debe estar vacio")
+	require.Panics(t, func() { heap.VerMax() }, "Heap vacio no puede ver elemento de mayor prioridad")
+	require.Panics(t, func() { heap.Desencolar() }, "Heap vacio  no puede desencolar")
+}
+
 func TestHeapVacio(t *testing.T) {
 	heap := TDAColaPrioridad.CrearHeap[string](cmpString)
-	require.Equal(t, 0, heap.Cantidad(), "Heap recien creado debe tener cantidad 0")
-	require.True(t, heap.EstaVacia(), "Heap recien creado debe estar vacio")
-	require.Panics(t, func() { heap.VerMax() }, "Heap recien creado no puede ver elemento de mayor prioridad")
-	require.Panics(t, func() { heap.Desencolar() }, "Heap recien creado no puede desencolar")
+	testsHeapVacio(t, heap)
 }
 
 // Test heap un elemento (probar todas las primitivas)
@@ -45,13 +50,29 @@ func TestHeapUnElemento(t *testing.T) {
 	require.Equal(t, "A", heap.VerMax(), "Heap con un elemento debe devolver ese unico elemento de mayor prioridad")
 	require.Equal(t, "A", heap.Desencolar(), "Heap con un elemento debe desencolar ese unico elemento")
 	// Luego de desencolar cumple con heap vacio
-	require.Equal(t, 0, heap.Cantidad(), "Heap luego de encolar y desencolar debe tener cantidad 0")
-	require.True(t, heap.EstaVacia(), "Heap luego de encolar y desencolar debe estar vacio")
-	require.Panics(t, func() { heap.VerMax() }, "Heap desencolado y vacio no puede ver elemento de mayor prioridad")
-	require.Panics(t, func() { heap.Desencolar() }, "Heap desencolado y vacio no puede desencolar")
+	testsHeapVacio(t, heap)
+}
+
+// cargarHeapArr(heap, arr) toma un heap y un array y carga los elementos del arreglo al heap
+func cargarHeapArr[T any](heap TDAColaPrioridad.ColaPrioridad[T], arr []T) {
+	for _, elem := range arr {
+		heap.Encolar(elem)
+	}
 }
 
 // Tests especificos
+func TestCasosEspecificos(t *testing.T) {
+	//arr := []int{1, 6, 3, 9, 5, 2, 4, 8, 7, 0}
+	//arrMax := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	//arrMin := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// Crear heaps de maximos y minimos y chequear vacios
+	heapMax := TDAColaPrioridad.CrearHeap[int](cmpIntMax)
+	heapMin := TDAColaPrioridad.CrearHeap[int](cmpIntMin)
+	testsHeapVacio(t, heapMax)
+	testsHeapVacio(t, heapMin)
+
+}
 
 // Test volumen
 
