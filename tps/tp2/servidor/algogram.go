f2f2b2fab5fcd83412db9226af68a1a706c4e01c
Ian Chen <chenian317@gmail.com>
2025-10-31 18:33:34 -0300

    agregar logica de registrar usuarios al servidor

--- tps/tp2/servidor/algogram.go
+++ tps/tp2/servidor/algogram.go
@@ -8,6 +8,7 @@ type AlgoGram struct {
 	sesion        *Usuario
 	usuarios      Diccionario.Diccionario[string, *Usuario] // Hash de nombre -> usuario
 	posts         Diccionario.Diccionario[int, *Post]       // Hash de id -> post
+	ordenRegistro Diccionario.Diccionario[string, int]      // Hash de nombre -> orden de la lista (para calc afinidad)
 	proximoPostId int
 }
 
@@ -17,12 +18,14 @@ func CrearServidor(usuarios []string) Servidor {
 		sesion:        nil,
 		usuarios:      Diccionario.CrearHash[string, *Usuario](func(a, b string) bool { return a == b }),
 		posts:         Diccionario.CrearHash[int, *Post](func(a, b int) bool { return a == b }),
+		ordenRegistro: Diccionario.CrearHash[string, int](func(a, b string) bool { return a == b }),
 		proximoPostId: 0,
 	}
+	servidor.registrarUsuarios(usuarios)
 	return &servidor
 }
 
-// Primitivas Algogram
+// *** Primitivas Algogram ***
 
 func (servidor *AlgoGram) Login(nombre string) {
 
@@ -46,4 +49,17 @@ func (servidor *AlgoGram) Likear(id int) {
 
 func (servidor AlgoGram) MostrarLikes(id int) {
 
+}
+
+// *** Funciones auxiliares ***
+
+func (servidor *AlgoGram) registrarUsuarios(usuarios []string) {
+	// Para cada usuario en la lista:
+	// - Crear usuario y guardar en hash al puntero
+	// - Guardar orden de registro de la lista
+	for i, nombre := range usuarios {
+		usuario := CrearUsuario(nombre)
+		servidor.usuarios.Guardar(nombre, usuario)
+		servidor.ordenRegistro.Guardar(nombre, i)
+	}
 }
