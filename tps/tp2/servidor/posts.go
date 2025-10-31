ede94ff0f433f094a048f2b7b750da12870f61fe
Ian Chen <chenian317@gmail.com>
2025-10-31 17:12:16 -0300

    configurar estructura de los posts y funcion de creacion

--- tps/tp2/servidor/posts.go
+++ tps/tp2/servidor/posts.go
@@ -1,1 +1,21 @@
 package servidor
+
+import diccionario "tdas/diccionario"
+
+type Post struct {
+	id            int
+	contenido     string
+	creador       string
+	cantidadLikes int
+	likes         diccionario.DiccionarioOrdenado[string, string]
+}
+
+// CrearPost (id, contenido, creador), toma nuevo ID de la publicacion, creador de publicacion,
+// y contenido de la publicacion. Devuelve puntero al post creado
+func CrearPost(nuevoId int, creadorPost, contenido string) *Post {
+	return &Post{
+		id:        nuevoId,
+		creador:   creadorPost,
+		contenido: contenido,
+	}
+}
