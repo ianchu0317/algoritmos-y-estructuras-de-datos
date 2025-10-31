787c7a2e55ae41c59f414dbfb4f9f298fd6091f2
Ian Chen <chenian317@gmail.com>
2025-10-31 17:16:17 -0300

    Crear estructura simple de usuario y funcion de creacion

--- tps/tp2/servidor/usuarios.go
+++ tps/tp2/servidor/usuarios.go
@@ -1,1 +1,17 @@
 package servidor
+
+import Heap "tdas/cola_prioridad"
+
+type Usuario struct {
+	nombre string
+	feed   Heap.ColaPrioridad[*Post]
+}
+
+// CrearUsuario(nombre) toma un nombre y devuelve un puntero a usuario
+func CrearUsuario(nombre string) *Usuario {
+	return &Usuario{
+		nombre:nombre,
+		feed: Heap.CrearHeap[*Post]()
+	}
+}
+
