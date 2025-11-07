502a025b6762bff46e5d8410cec6ddd0d3de7a5d
Ian Chen <chenian317@gmail.com>
2025-11-07 16:28:23 -0300

    implementar unionfind para utilizar en mst

--- ejemplos/grafos/tdas.py
+++ ejemplos/grafos/tdas.py
@@ -30,6 +30,7 @@ class Cola:
         return self.cantidad
 
 
+
 class Pila:
     def __init__(self):
         self.datos = deque()
@@ -69,3 +70,30 @@ class Heap:
     def desapilar(self):
         if not self.max_heap:
             return heapq.heappop()
+
+
+
+class UnionFind:
+    def __init__(self, elems: list):
+        self.grupos = dict()
+        for e in elems:
+            self.grupos[e] = e        
+    
+    def find(self, v):
+        """
+        UnionFInd.find(v) devuelve el grupo donde pertenece V
+        """
+        if self.grupos[v] == v:
+            return v
+        # Actualizar el valor y devolver valor del grupo
+        self.grupos[v] = self.find(self.grupos[v])
+        return self.grupos[v]
+    
+    def union(self, u, v):
+        """
+        UnionFind.union(u, v) une a U al grupo de V
+        """
+        nuevo_grupo = self.find(u)
+        otro = self.find(v)
+        self.grupos[otro] = nuevo_grupo
+    
