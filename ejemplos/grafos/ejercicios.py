c135527ae85a31ac4c2216d9caf931d0cde339a4
Ian Chen <chenian317@gmail.com>
2025-11-15 20:46:29 -0300

    ejercicio 2 de parcialito 3

--- ejemplos/grafos/ejercicios.py
+++ ejemplos/grafos/ejercicios.py
@@ -1,5 +1,6 @@
 from grafos import Grafo
 from tdas.cola import Cola
+from tdas.heap import Heap
 import random
 
 """
@@ -547,3 +548,73 @@ def ej3_p3_1c2025(grafo: Grafo, origen):
         if dist == max_dist:
             vertices.append(v)
     return vertices
+
+
+
+"""
+Ejercicio 2, parcialito 3 1c2025:
+
+En una pequeña ciudad costera, todos los habitantes usan el AlgoMaps, una aplicación que les ayuda a decidir sus rutas de
+viaje diarias. Conociendo su funcionamiento, sabemos que siempre ofrece la ruta del camino más corto de un punto a otro
+dadas las rutas existentes, sabiendo que cuenta con la información de la distancia cubierta por cada camino entre punto y
+punto. En el futuro se planifican ciertas obras en diferentes puntos de la ciudad que seguramente afecten a la red de transporte.
+Para evitar afectar en demasía a los ciudadanos, se busca analizar la información disponible para planificar las obras. Sería
+deseable evitar bloquear el transporte de los puntos más importantes de la ciudad.
+a. Explicar cómo la información disponible se puede modelar con un grafo, y explicar sus características. 
+¿Qué información debemos solicitarle a AlgoMaps?
+b. Desarrollar una función puntosImportantes(grafo, k) que reciba el grafo definido en el punto (a) y que devuelva los k
+puntos más importantes en la ciudad, según lo modelado. Indicar y justificar su complejidad.
+"""
+
+
+def ej2_p3_1c2025(grafo: Grafo, k: int) -> list:
+    """
+    **Modelado**
+    Se puede modelar al problema con un grafo dirigido y pesado donde:
+    - Vertices: los distintos puntos de la ciudad
+    - Aristas: Los distintos caminos de la ciudad
+    - Peso de aristas: Tiempo de recorrido de un camino
+    
+    Teniendo en cuenta eso, los puntos importantes son los puntos que con mas frecuencia están en los caminos minimos,
+    es decir los k-puntos con mayor centralidad del grafo 
+    
+    **Procedimiento**
+    Obtener los puntos más transitados / importantes (calcular centralidad)
+    Ordenar los puntos obtenidos de menor a mayor
+    Obtener los k puntos mas importantes
+    
+    -> Se cuenta con distancia 
+    
+    **Complejiad**
+    """
+    centralidad = dict()
+    for v in grafo:
+        centralidad[v] = 0
+    
+    for v in grafo:
+        for w in grafo:
+            if w == v:
+                continue
+            _, padres = dijkstra(grafo, v, w)
+            
+            if not padres:
+                continue
+            actual = padres[w]
+            while actual != v:
+                centralidad[actual] += 1
+                actual = padres[actual]
+    
+    heap = Heap()
+    i = 0
+    for v, freq in centralidad.items():
+        if i < k:
+            heap.encolar((freq, v))
+            i += 1
+        elif heap.ver_primero()[0] < freq:
+            heap.desencolar()
+            heap.encolar((freq, v))
+    resultado = []
+    while not heap.esta_vacia():
+        _, v = heap.desencolar()
+        resultado.append(v)
+    return resultado
