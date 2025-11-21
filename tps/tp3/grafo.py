67f2a2fb959cd08f480a1422b0b590c5b4fb39b1
Ian Chen <chenian317@gmail.com>
2025-11-21 18:02:04 -0300

    corregir documentaciones

--- tps/tp3/grafo.py
+++ tps/tp3/grafo.py
@@ -2,34 +2,59 @@
 class Grafo:
     def __init__(self, es_dirigido: bool):
         """
-        Grafo(es_pesado, es_dirigido) toma dos parametros y devuelve un grafo dirigido y/o pesado
+        Grafo(es_dirigido) toma dos parametros y devuelve un grafo dirigido y/o pesado
+        
+        Ejemplo:
+            Grafo(es_dirigido=False) -> Crea un grafo no dirigido
+            Grafo(es_dirigido=True) -> Crea un grafo dirigido
         """
         self.vertices = dict()      # Diccionario de listas
         self.pesos = dict()         # Diccionario de diccionario
         self.es_dirigido = es_dirigido
     
     
+    def hay_vertice(self, v) -> bool:
+        """
+        hay_vertice(v) toma un vertice 'v' y devuelve true si el vértice está en grafo. False en caso contrario
+        """
+        return v in self.vertices
+    
+    
+    def hay_arista(self, v, w) -> bool:    
+        """
+        hay_arista(v,w) toma dos vertices y devuelve True en caso de existir arista entre 'v' y 'w'.
+        False en caso contrario.
+        Si alguno de los vertices no existe devuelve error con "No existe vertice en grafo"
+        """
+        if not self.hay_vertice(v) or not self.hay_vetice(w):
+            print("No existe vertice en grafo")
+            return
+        return w in self.vertices[v]
+    
+    
     def agregar_vertice(self, v):
         """
-        agregar_vertice(v) toma un vertice 'v' y lo agrega al grafo.
+        grafo.agregar_vertice(v) toma un vertice 'v' y lo agrega al grafo.
         Si vertice ya existe devuelve error "Vertice ya esta en grafo"
         """
-        if v in self.vertices:
-            print("Vertice ya esta en grafo")
-            return
+        if self.hay_vertice(v):
+            raise ValueError("Vertice ya esta en grafo")
         self.vertices[v] = []
         self.pesos[v] = dict()
     
         
     def agregar_arista(self, v, w, peso_vw: 0):
         """
-        agregar_arista(v, w, peso_vw) toma dos vertices 'v' 'w' y los une.
-        Por defecto el peso_vw es 0 para grafo no pesado.
+        grafo.agregar_arista(v, w, peso_vw) toma dos vertices 'v' 'w' y los une.
+        Si se quiere operar con grafo no pesado, no pasarle nada al parametro peso_vw (por defecto 0).
+        Si algun vertice no existe devuelve "Algun vertice no existe"
         Si ya existe arista entonces devuelve "Ya existe arista".
         """
-        if v not in self.vertices or w not in self.vertices:
-            print("Algun vertice ingresado no existe")
-            return
+        if not self.hay_vertice(v) or not self.hay_vertice(w):
+            raise ValueError("Algun vertice no existe")
+        if w in self.vertices[v]:
+            raise ValueError("Ya existe arista")
+
         self.vertices[v].append(w)
         self.pesos[v][w] = peso_vw
         if not self.es_dirigido:
@@ -39,20 +64,21 @@ class Grafo:
         
     def adyacentes(self, v) -> list:
         """
-        adyacentes(v) toma un vertice v y devuelve los adyacentes del vertice (aristas)
+        adyacentes(v) toma un vertice v y devuelve los adyacentes del vertice (aristas).
+        Si no existe el vertice devuelve "No existe vertice en grafo".
         """
         if v not in self.vertices:
-            print("No existe vertice en grafo")
-            return None
+            raise ValueError("No existe vertice en grafo")
         return self.vertices[v]
     
     
     def peso_arista(self, v, w):
         """
-        peso_arista(v, w) toma dos airistas y devuelve su peso si es un grafo pesado
+        peso_arista(v, w) toma dos airistas y devuelve su peso si es un grafo pesado.
+        Si algun vertice ingresado no existe devuelve "Algun vertice no existe"
         """
         if v not in self.vertices or w not in self.vertices:
-            print("Algun vertice ingresado no existe")
+            print("Algun vertice no existe")
             return
         return self.pesos[v][w]
     
@@ -64,34 +90,18 @@ class Grafo:
         return self.vertices.keys()
 
     
-    def hay_arista(self, v, w) -> bool:    
-        """
-        hay_arista(v,w) toma dos vertices y devuelve True en caso de existir arista entre 'v' y 'w'.
-        False en caso contrario.
-        Si alguno de los vertices no existe devuelve error con "Vertice ingresado no existe"
-        """
-        if v not in self.vertices or w not in self.vertices:
-            print("Verticec ingresado no existe")
-            return
-        return w in self.vertices[v]
-    
-    
-    def hay_vertice(self, v) -> bool:
-        """
-        hay_vertice(v) toma un vertice 'v' y devuelve true si el vértice está en grafo. False en caso contrario
-        """
-        return v in self.vertices
+
         
     
     def borrar_vertice(self, v):
         """
         borrar_vertice(v) toma un vertice y borrar sus aristas. 
         Si es dirigido también borra sus apariciones en otros vertices.
-        Si no existe vertice en grafo, no hace nada
+        Si no existe vertice en grafo, devuelve "No existe vertice en grafo"
         """
         
         if not self.hay_vertice(v):
-            print("Vertice no esta en grafo")
+            print("No existe vertice en grafo")
             return
         self.vertices.pop(v)
         # Eliminar grado de entrada
@@ -104,12 +114,14 @@ class Grafo:
             if v in peso_wv:
                 peso_wv.pop(v)
     
+    
     def borrar_arista(self, v, w):
         """
         borrar_arista(v, w) toma dos vertices del grafo y SI tienen aristas borrar la arista, sino nada
+        Si vertice ingresado no esta en grafo, deveuvle "No existe vertice en grafo"
         """
         if not self.hay_vertice(v) or not self.hay_vertice(w):
-            raise ValueError("Vertice ingresado no esta en grafo")
+            raise ValueError("No existe vertice en grafo")
 
         if w in self.vertices[v]:
             self.vertices[v].remove(w)
