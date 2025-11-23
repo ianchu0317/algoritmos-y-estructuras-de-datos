4d783287ba302bad758a80803193ea2fc7ed0907
Ian Chen <chenian317@gmail.com>
2025-11-22 21:17:41 -0300

    agregar implementacion camino minimo

--- tps/tp3/biblioteca.py
+++ tps/tp3/biblioteca.py
@@ -2,6 +2,52 @@ from grafo import Grafo
 from collections import deque
 import heapq
 
+
+def camino_minimo(grafo: Grafo, origen, destino) -> list:
+    """
+    Devuelve una lista con el camino mínimo desde origen hasta destino de un grafo no dirigido.
+    En caso de no existir el camino se devuelve una lista vacía.
+    
+    **Parámetros**: origen y destino
+    
+    **Complejidad**: O(V + E)
+    """
+    
+    padres = dict()
+    cola = deque()
+    visitados = set()
+    
+    cola.append(origen)
+    padres[origen] = None
+    visitados.add(v)
+    
+    while len(cola) > 0:
+        v = cola.popleft()
+
+        if v == destino:
+            return reconstruir_camino(padres, origen, destino)
+        
+        for w in grafo.adyacentes(v):
+            if w not in visitados:
+                visitados.add(w)
+                cola.append(w)
+                padres[w] = v
+    
+    # Si llega hasta acá no encontró camino
+    return []
+
+
+def reconstruir_camino(padres: dict, ini, fin) -> list:
+    """
+    Toma un diccionario de padres y devuelve una lista con camino desde 'ini' hasta 'fin'
+    
+    Complejidad O(V)
+    """
+    camino = [fin]
+    while fin != ini:
+        fin = padres[fin]
+        camino.append(fin)
+    return list(reversed(camino))
     
     
 if __name__ == '__main__':
