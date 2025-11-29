68e7f6bc9e50645a940574f6d280565e85639d3b
Ian Chen <chenian317@gmail.com>
2025-11-29 18:57:17 -0300

    implementar leer tsv

--- tps/tp3/main.py
+++ tps/tp3/main.py
@@ -12,7 +12,14 @@ if __name__ == '__main__':
     # Leer tsv
     with open(archivo_tsv, "r") as archivo:
         contenido = archivo.read()
-        print("- linea es: ", contenido)
-    # Cargar servidor
+    
+    # Cargar servidor -> Para cada linea de tsv, separar a lista
+    for dato in contenido.split("\n"):
+        linea_lista_tsv = dato.split("\t")
+        netstat.agregar_post(linea_lista_tsv[0], linea_lista_tsv[1:])
+    
+    # Esperar comandos
+    cmd = input()
+    print(cmd)
 
 
