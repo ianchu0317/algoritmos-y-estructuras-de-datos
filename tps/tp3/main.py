a60af5cf577ab8b1c94adb550f5966eef5f6b72f
Ian Chen <chenian317@gmail.com>
2025-11-30 01:07:44 -0300

    agregar leer tsv opcional

--- tps/tp3/main.py
+++ tps/tp3/main.py
@@ -71,17 +71,19 @@ def operar(netstat: Netstats, operacion, argumentos):
 
 if __name__ == '__main__':
     netstat = Netstats()
-    archivo_tsv = sys.argv[1]  # Suponer que se pasa bien argumento
     
-    # Leer tsv
-    with open(archivo_tsv, "r") as archivo:
-        contenido = archivo.read()
-    
-    # Cargar servidor -> Para cada linea de tsv, separar a lista
-    for dato in contenido.split("\n"):
-        linea_lista_tsv = dato.split("\t")
-        netstat.agregar_post(linea_lista_tsv[0], linea_lista_tsv[1:])
-    
+    # Leer TSV opcional
+    if len(sys.argv) > 1:
+        archivo_tsv = sys.argv[1]  # Suponer que se pasa bien argumento
+        # Leer tsv
+        with open(archivo_tsv, "r") as archivo:
+            contenido = archivo.read()
+        
+        # Cargar servidor -> Para cada linea de tsv, separar a lista
+        for dato in contenido.split("\n"):
+            linea_lista_tsv = dato.split("\t")
+            netstat.agregar_post(linea_lista_tsv[0], linea_lista_tsv[1:])
+        
     # Esperar comandos -> Hasta que no haya inputs entonces break
     while True:
         try:
